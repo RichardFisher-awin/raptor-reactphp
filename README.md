f570ce2d98e4fd2798fa4338be733c961364925e
Richard Fisher <richard.fisher@affiliatewindow.com>
2018-09-29 12:00:25 +0100

    Routing using nikic/fast-route

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # raptor-reactphp
+
+TODO:
+
+- DI container, with a config file to describe dependencies
+- Make the dispatcher use a config file and the DI container rather than hard-coded routes
+- Endpoint to handle status.txt
+- MySQL healthcheck
--- src/app.php
+++ src/app.php
@@ -3,19 +3,38 @@ use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
 use React\Http\Response;
 use React\Http\Server;
+use FastRoute\RouteCollector;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $loop = Factory::create();
-$server = new Server(function (ServerRequestInterface $request) {
-    return new Response(
-        200,
-        array(
-            'Content-Type' => 'text/plain'
-        ),
-        "Hello world\n"
-    );
+
+$indexPage = function() {
+    return new Response(200, ['Content-Type' => 'text/plain'], "Hello world\n");
+};
+
+$greeter = function(ServerRequestInterface $request, $name) {
+    return new Response(200, ['Content-Type' => 'text/plain'], "Hello $name\n");
+};
+
+$dispatcher = FastRoute\simpleDispatcher(function(RouteCollector $routes) use ($indexPage, $greeter) {
+    $routes->addRoute('GET', '/', $indexPage);
+    $routes->addRoute('GET', '/hello/{name:\w+}', $greeter);
 });
+
+$server = new Server(function (ServerRequestInterface $request) use ($dispatcher) {
+    $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getUri()->getPath());
+
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            return new Response(404, ['Content-Type' => 'text/plain'],  'Not found');
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            return new Response(405, ['Content-Type' => 'text/plain'],  'Method not allowed');
+        case FastRoute\Dispatcher::FOUND:
+            return $routeInfo[1]($request, ... array_values($routeInfo[2]));
+    }
+});
+
 $socket = new \React\Socket\Server(isset($argv[1]) ? $argv[1] : '0.0.0.0:80', $loop);
 $server->listen($socket);
 echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
